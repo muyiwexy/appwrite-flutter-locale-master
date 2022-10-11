fee5b7203faf09ea3143070c0bc1390e1510b35c
Femi-ige Muyiwa <84715593+muyiwexy@users.noreply.github.com>
2022-10-11 16:50:18 +0100

    Update authenticate.dart

--- lib/auth/authenticate.dart
+++ lib/auth/authenticate.dart
@@ -8,13 +8,16 @@ import '../data/userdata.dart';
 class Authenticate extends ChangeNotifier {
   Client client = Client(); // create a new Appwrite client
   Account? account; //call the Appwrite acccount API
+  Locale? locale; //call the Appwrite Locale API
   late bool _isLoggedin; //Creates a boolean instance to check if user is logged in
   late bool _createaccount; //Creates a boolean instance to check if user is logged in
   User? _user; // calls our model class for userdata
+  LocaleData? _localeData; // calls our model class for userdata
 
   bool get isLoggedin => _isLoggedin; //getter for _isLoggedin
   bool get createaccount => _createaccount;  //getter for _createaccount
   User? get user => _user;  //getter for _user
+  LocaleData? get localedata => _localeData;  //getter for _localeData
 
   Authenticate() {
     _init(); // This function creates our appwrite instance
@@ -28,16 +31,35 @@ class Authenticate extends ChangeNotifier {
     client
         .setEndpoint(Appconstants.endpoint)
         .setProject(Appconstants.projectid);
-
     account = Account(client);  // create a new Appwrite account
+    locale = Locale(client);  // create a new Appwrite locale
     _checkisLoggedin(); // checks two future functions (_user and _localeData)
   }
 
   _checkisLoggedin() async {
     try {
       _user = await _getaccount(); //_getaccount function checks if the user is already logged in and if true, load the userdata
+      _localeData = await _localise(); //_localise function checks for the user's locale and loads it once the user is logged in
       _isLoggedin = true;
       notifyListeners();
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future _localise() async {
+    try {
+      var result = await locale?.get();
+      _isLoggedin = true;
+      _user = await _getaccount();
+      notifyListeners();
+      if (result?.ip != null) {
+        var data = jsonEncode(result?.toMap());
+        var jsondata = jsonDecode(data);
+        return LocaleData.fromJson(jsondata);
+      } else {
+        return null;
+      }
     } catch (e) {
       print(e);
     }
@@ -59,10 +81,15 @@ class Authenticate extends ChangeNotifier {
     }
   }
 
+  localisation() async {
+    return await locale?.get();
+  }
+
   login(String email, String password) async {
     try {
       await account!.createEmailSession(email: email, password: password); // create a new email session with respect to the parameters provided (if an account exist, then we will be able to create a new session)
-      _user = await _getaccount();
+      _user = await _getaccount(); 
+      _localeData = await _localise();
       _isLoggedin = true;
       notifyListeners();
     } catch (e) {
@@ -85,4 +112,14 @@ class Authenticate extends ChangeNotifier {
     }
   }
 
+  signout() async {
+    try {
+      await account?.deleteSession(sessionId: 'current'); // delete the current logged in user session
+      _isLoggedin = false; // sets _isLoggedin to false thus the page won't automatically be logged in when re initialised
+      _user = await _getaccount();
+      notifyListeners();
+    } catch (e) {
+      print(e);
+    }
+  }
 }
